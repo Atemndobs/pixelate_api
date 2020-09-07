ff2c480c8ad02180196785c18182eb1cabb97539
@Atemndobs <atemndobs@yahoo.com>
2020-09-07 20:24:24 +0200

    add geodata

--- app/Http/Controllers/User/SettingsController.php
+++ app/Http/Controllers/User/SettingsController.php
@@ -9,6 +9,7 @@ use App\Rules\MatchOldPassword;
 use Grimzy\LaravelMysqlSpatial\Types\Point;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use PhpParser\Node\Expr\Cast;
 
 class SettingsController extends Controller
 {
@@ -16,39 +17,23 @@ class SettingsController extends Controller
     {
         $user = auth()->user();
 
-
-
-
             $this->validate($request, [
                 'name' => ['required'],
                 'tagline' => ['required'],
                 'formatted_address' => ['required'],
                 'about' => ['required', 'string', 'min:20'],
                 'available_to_hire' => ['required'],
-                'location.latitude' => ['required', 'int', 'min:-90', 'max:90'],
-                'location.longitude' => ['required', 'int', 'min:-180', 'max:180'],
+                'location.latitude' => ['required', 'numeric', 'min:-90', 'max:90'],
+                'location.longitude' => ['required', 'numeric', 'min:-180', 'max:180'],
             ]);
 
 
-        $location = new Point($request->location['latitude'], $request->location['longitude']);
-
-        $lat = $location->getLat();
-        $long = $location->getLng();
-
-        // CREATE SPATIAL REFERENCE SYSTEM 104326 NAME 'WGS 84 N-E' DEFINITION 'GEOGCS["WGS 84",DATUM["World Geodetic System 1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.017453292519943278,AUTHORITY["EPSG","9122"
-
-       # $query = "update `users` set `location` = ST_GeomFromText('POINT($lat $long)') where `id` = $user->id";
-        $query = "update `users` set `location` = ST_GeomFromText('POINT($lat $long)') where `id` = $user->id";
+        $points = new Point($request->location['latitude'], $request->location['longitude']);
 
 
-
-        DB::statement(
-            $query
-        );
-
-        $data = DB::select('select * from users');
-
-
+        $lat = $points->getLat();
+        $long = $points->getLng();
+        $location = "ST_GeomFromText('POINT($lat $long)')";
 
         $user->update([
             'name' => $request->name,
@@ -56,10 +41,16 @@ class SettingsController extends Controller
             'formatted_address' => $request->formatted_address,
             'about' => $request->about,
             'available_to_hire' => $request->available_to_hire,
-            'location'=>$location
+            'location' => DB::raw($location)
         ]);
 
-        return new UserResource($user);
+        $query = "SELECT ST_AsGeoJSON(location) as location FROM `users`";
+        $updated_location = DB::select($query);
+
+        $updated_user = new UserResource($user);
+        $updated_user->location = json_decode($updated_location[0]->location);
+
+        return $updated_user;
     }
 
     public function updatePassword(Request $request)
