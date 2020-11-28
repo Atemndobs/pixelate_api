782deeec5df29f8dbcf4fef1de2292e11eba43a6
Bertrand Ndobegang Atemkeng <b.atemkeng@fashionette.de>
2020-11-28 11:55:23 +0100

    wip

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -130,8 +130,10 @@ class RegisterController extends Controller
      */
     protected function registered(Request $request, $user)
     {
-        if (response()->status() === 503){
-            return json_encode(response()->getContent(), JSON_THROW_ON_ERROR);
+        try {
+            $this->register($request);
+        }catch (\Exception $e){
+            return json_encode($e->getMessage());
         }
        return response()->json($user, 200);
     }
