f3ccf276d049de125ff7fbac12182f5add48bc4d
Bertrand Ndobegang Atemkeng <b.atemkeng@fashionette.de>
2020-12-21 18:35:16 +0100

    start

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -32,7 +32,7 @@ return [
 
         // pusher
 
-        'pusher' => [
+/*        'pusher' => [
             'driver' => 'pusher',
             'key' => env('PUSHER_APP_KEY'),
             'secret' => env('PUSHER_APP_SECRET'),
@@ -47,12 +47,12 @@ return [
                 // 'useTLS' => true
 
             ],
-        ],
+        ],*/
 
         // laravel-websocket
 
 
-/*           'pusher' => [
+           'pusher' => [
             'driver' => 'pusher',
             'key' => env('PUSHER_APP_KEY'),
             'secret' => env('PUSHER_APP_SECRET'),
@@ -73,7 +73,7 @@ return [
                 'port' => 6001,
                 'scheme' => 'http'
             ],
-        ],*/
+        ],
 
 
         'redis' => [
